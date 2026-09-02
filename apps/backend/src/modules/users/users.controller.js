2e686f065bad8c9bbbefcc6d265e3cdf9e2c360b
LeeSeoJin <dltjwls5669@naver.com>
2026-09-02 23:46:08 +0900

    docs: users 모듈 함수 단위 주석 추가

--- apps/backend/src/modules/users/users.controller.js
+++ apps/backend/src/modules/users/users.controller.js
@@ -1,6 +1,7 @@
-// # 기능: USER-001~004: 가입 완료/내정보/프로필수정/탈퇴 HTTP req/res 처리
+// 기능: USER-001~004: 가입 완료/내정보/프로필수정/탈퇴 HTTP req/res 처리
 import * as usersService from './users.service.js'
 
+// 내 정보 조회
 export async function getMe(req, res, next) {
     try {
         const user = await usersService.getMe(req.user.id)
